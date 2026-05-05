f5b279d69171a2e317ad2c618d478eea20a3e2bf
Denis Zlatanov <bombata053@protonmail.com>
2026-05-05 23:37:40 +0300

    Add: default required mason and treesitter packages

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -4,5 +4,56 @@ return {
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
     },
+	config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "bashls",
+                "clangd",
+                "gopls",
+                "pylsp",
+                "ts_ls",
+                "html",
+                "cssls",
+                "lua_ls",
+                "cmake",
+                "efm",
+            },
+            automatic_installation = true,
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                -- go
+                "gofumpt",
+                "goimports-reviser",
+                "golines",
+
+                -- c/cpp
+                "clang-format",
+
+                -- python
+                "black",
+                "isort",
+
+                -- js
+                "prettier",
+
+                -- bash
+                "shfmt",
+
+                -- lua
+                "stylua",
+
+                -- git
+                "gitlint",
+
+                -- markdown
+                "markdownlint",
+            },
+            auto_update = false,
+            run_on_start = true,
+        })
+    end,
 }
